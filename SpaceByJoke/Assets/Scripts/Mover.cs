db06c4e8ee32575f1ff4ac9f4e1c494c2465c90c
Wutthichai Kammasit <wutthichai.kamm@bumail.net>
2016-02-09 17:33:37 +0700

    Space Lab Video 9

--- SpaceByJoke/Assets/Scripts/Mover.cs
+++ SpaceByJoke/Assets/Scripts/Mover.cs
@@ -1,11 +1,17 @@
-﻿using UnityEngine;
+﻿
+using UnityEngine;
 using System.Collections;
-public class Mover : MonoBehaviour {
+
+public class Mover : MonoBehaviour
+{
 	public float speed;
-	private Rigidbody rb;
-	void Start()
+	public Rigidbody rb;
+	void Start ()
 	{
-		rb = GetComponent<Rigidbody> ();
+		rb = GetComponent<Rigidbody>();
 		rb.velocity = transform.forward * speed;
+
 	}
-}
+
+
+}﻿
