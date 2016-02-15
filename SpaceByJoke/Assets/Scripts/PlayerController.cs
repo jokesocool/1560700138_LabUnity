7c31cf00a7e88394d28ecd2c11d179e61a0fe20c
Wutthichai Kammasit <wutthichai.kamm@bumail.net>
2016-02-15 16:49:53 +0700

    Video Lab Video 13

--- SpaceByJoke/Assets/Scripts/PlayerController.cs
+++ SpaceByJoke/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+
 [System.Serializable]
 public class Boundary
 {
@@ -10,26 +11,39 @@ public class PlayerController : MonoBehaviour
 	public float speed;
 	public float tilt;
 	public Boundary boundary;
+
 	public GameObject shot;
 	public GameObject shotSpawn; 
 	public float fireRate;
+
 	private float nextFire;
+	private AudioSource audioSource ;
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource >();
+	}
 	void Update ()
 	{
-		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
 			nextFire = Time.time + fireRate;
 			Instantiate (shot, shotSpawn.transform.position, shotSpawn.transform.rotation);
+			audioSource.Play();
 		}
 	}
 	void FixedUpdate ()
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
+
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		GetComponent<Rigidbody>().velocity = movement * speed;
+
 		GetComponent<Rigidbody>().position = new Vector3
 			(
-				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 
+				0.0f, 
+				Mathf.Clamp (GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
 			);
 		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
 	}
