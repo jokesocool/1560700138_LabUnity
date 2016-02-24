bda5b75ddc903f2f3144d298991ca843063d7f36
WUTTHICHAI KAMMASIT <wutthichai.kamm@bulive.net>
2016-02-24 12:54:25 +0700

    Jam Lab V2

---
+++ Test Jam Lab 1/Assets/Again.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Again : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void DoplayAgain(){
+		Application.LoadLevel (0);
+	}
+}
---
+++ Test Jam Lab 1/Assets/Kadod.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kadod : MonoBehaviour {
+	public Rigidbody rb;
+	public GameObject playerDeadprefab;
+	public GameObject button;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		if (Input.GetButtonDown("Jump"))
+			rb.velocity = new Vector3(0, 2, 0);
+		
+	}
+	void Update(){
+		if (rb.velocity.y < 0) {
+			lastVelocity = rb.velocity.y;
+		}
+	}
+	float lastVelocity;
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "floor") {
+			//			float currentvelocity = rb.velocity.y;
+			Debug.LogFormat("currentvelocity = {0}",lastVelocity);
+			rb.velocity = new Vector3(0,-lastVelocity, 0);
+			
+		}
+		else if(collision.gameObject.tag == "Ceil") {
+			Instantiate(playerDeadprefab,this.gameObject.transform.position,Quaternion.identity);
+			Destroy(this.gameObject);
+			button.SetActive(true);
+		}
+	}
+
+	
+	
+}
