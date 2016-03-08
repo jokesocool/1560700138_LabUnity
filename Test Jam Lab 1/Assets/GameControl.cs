7344cab1125ea19490131c9ffb6c086d82088789
Wutthichai Kammasit <wutthichai.kamm@bumail.net>
2016-03-09 01:14:08 +0700

    Jam Lab V3

---
+++ Test Jam Lab 1/Assets/GameControl.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	// Use this for initialization
+	void Start () {
+		SpawnWaves ();
+	}
+	
+	// Update is called once per frame
+	void SpawnWaves () {
+		Vector3 spawnPosition = new Vector3 (spawnValues.x, spawnValues.y, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (hazard, spawnPosition, spawnRotation);
+	}
+}
