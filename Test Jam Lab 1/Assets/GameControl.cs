34e209f1c1d0bc94f5abdd72d4c6acdbbfd385fb
WUTTHICHAI KAMMASIT <wutthichai.kamm@bulive.net>
2016-03-09 12:55:32 +0700

    Jam Lab V4

--- Test Jam Lab 1/Assets/GameControl.cs
+++ Test Jam Lab 1/Assets/GameControl.cs
@@ -2,15 +2,30 @@
 using System.Collections;
 
 public class GameControl : MonoBehaviour {
+	float speedX = 0;
+	float speedY = 1;
+	float speedZ = 0;
+	public Rigidbody rb;
+
 	public GameObject hazard;
 	public Vector3 spawnValues;
+
 	// Use this for initialization
 	void Start () {
 		SpawnWaves ();
 	}
 	
 	// Update is called once per frame
-	void SpawnWaves () {
+	void Update () {
+		rb = GetComponent<Rigidbody> ();
+		transform.Translate (new Vector3 (speedX, speedY, speedZ) * Time.deltaTime);
+	}
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.tag == "Wall") {
+			Destroy (this.gameObject);
+		}
+	}
+	void SpawnWaves(){
 		Vector3 spawnPosition = new Vector3 (spawnValues.x, spawnValues.y, spawnValues.z);
 		Quaternion spawnRotation = Quaternion.identity;
 		Instantiate (hazard, spawnPosition, spawnRotation);
--- Test Jam Lab 1/Assets/Kadod.cs
+++ Test Jam Lab 1/Assets/Kadod.cs
@@ -22,17 +22,22 @@ public class Kadod : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.tag == "floor") {
 			//			float currentvelocity = rb.velocity.y;
-			Debug.LogFormat("currentvelocity = {0}",lastVelocity);
-			rb.velocity = new Vector3(0,-lastVelocity, 0);
+			Debug.LogFormat ("currentvelocity = {0}", lastVelocity);
+			rb.velocity = new Vector3 (0, -lastVelocity, 0);
 			
-		}
-		else if(collision.gameObject.tag == "Ceil") {
-			Instantiate(playerDeadprefab,this.gameObject.transform.position,Quaternion.identity);
-			Destroy(this.gameObject);
-			button.SetActive(true);
+		} else if (collision.gameObject.tag == "Ceil") {
+			Instantiate (playerDeadprefab, this.gameObject.transform.position, Quaternion.identity);
+			Destroy (this.gameObject);
+			button.SetActive (true);
+		} else if (collision.gameObject.tag == "Wall") {
+			Instantiate (playerDeadprefab, this.gameObject.transform.position, Quaternion.identity);
+			Destroy (this.gameObject);
+			button.SetActive (true);
 		}
 	}
-
-	
-	
+	void OnCollisionExit(Collision collision){
+		if (collision.gameObject.tag == "Cyl") {
+			rb.velocity= new Vector3 (0,rb.velocity.y,0);
+		}
+	}
 }
