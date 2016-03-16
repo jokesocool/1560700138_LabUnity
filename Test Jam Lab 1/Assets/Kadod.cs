9e357c94f5d321007bfad4b32bfc35ed3f402805
WUTTHICHAI KAMMASIT <wutthichai.kamm@bulive.net>
2016-03-16 13:25:26 +0700

    Jam Lab V5

--- Test Jam Lab 1/Assets/Kadod.cs
+++ Test Jam Lab 1/Assets/Kadod.cs
@@ -1,18 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Kadod : MonoBehaviour {
 	public Rigidbody rb;
-	public GameObject playerDeadprefab;
-	public GameObject button;
+	public GameObject Button;
+	public GameObject PlayerDeadPrefab;
+	private int count;
+	public Text countText;
+	public void Jump (){
+		rb.velocity = new Vector3(0, 5, 0);
+	}
 	void Start() {
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		countText.text = "score : " +count.ToString ();
 	}
-	void FixedUpdate() {
-		if (Input.GetButtonDown("Jump"))
-			rb.velocity = new Vector3(0, 2, 0);
-		
-	}
+
+
 	void Update(){
 		if (rb.velocity.y < 0) {
 			lastVelocity = rb.velocity.y;
@@ -21,23 +26,29 @@ public class Kadod : MonoBehaviour {
 	float lastVelocity;
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.tag == "floor") {
-			//			float currentvelocity = rb.velocity.y;
+//			float currentvelocity = rb.velocity.y;
 			Debug.LogFormat ("currentvelocity = {0}", lastVelocity);
 			rb.velocity = new Vector3 (0, -lastVelocity, 0);
-			
-		} else if (collision.gameObject.tag == "Ceil") {
-			Instantiate (playerDeadprefab, this.gameObject.transform.position, Quaternion.identity);
+		} else if (collision.gameObject.tag == "ceil") {
+			Instantiate(PlayerDeadPrefab,this.gameObject.transform.position,Quaternion.identity);
 			Destroy (this.gameObject);
-			button.SetActive (true);
-		} else if (collision.gameObject.tag == "Wall") {
-			Instantiate (playerDeadprefab, this.gameObject.transform.position, Quaternion.identity);
-			Destroy (this.gameObject);
-			button.SetActive (true);
+			Button.SetActive(true);
 		}
 	}
-	void OnCollisionExit(Collision collision){
-		if (collision.gameObject.tag == "Cyl") {
-			rb.velocity= new Vector3 (0,rb.velocity.y,0);
+	void OnCollisionExit(Collision collision) {
+		if (collision.gameObject.tag == "cyl") {
+			rb.velocity = new Vector3 (0,rb.velocity.y,0);
 		}
 	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("score+1")) {
+			Debug.Log ("Check");
+
+			count++;
+			countText.text = "score : " + count.ToString ();
+
+		}
+	}
+
 }
--- Test Jam Lab 1/Assets/canvas.cs
+++ Test Jam Lab 1/Assets/canvas.cs
@@ -1,37 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameControl : MonoBehaviour {
+public class canvas : MonoBehaviour {
 	public GameObject hazard;
 	public int hazardCount;
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
 
+	// Use this for initialization
 	void Start () {
-		StartCoroutine (SpawnWaves ());
+		Debug.Log("hit");
+		StartCoroutine (SpawnWaves());
 	}
-
+	
+	// Update is called once per frame
 	void Update () {
-		
 	}
 	public void DoPlayAgain(){
 		Application.LoadLevel (0);
-	
 	}
 	IEnumerator SpawnWaves()
 	{
 		yield return new WaitForSeconds (startWait);
 		while (true) {
 			for (int i = 0; i < hazardCount; i++) {
-				Vector3 spawnPosition = new Vector3 (4, 0, 0);
+				float random = Random.Range(max:3.6f,min:-2.3f);
+				Vector3 spawnPosition = new Vector3 (6,random,0);
 				Quaternion spawnRotation = Quaternion.identity;
-				GameObject hazardShoot = (GameObject)Instantiate (hazard, spawnPosition, spawnRotation);
-				hazardShoot.transform.Rotate (new Vector3 (0, 0, 1), 90);
+				GameObject hazardshoot = (GameObject)Instantiate (hazard, spawnPosition, spawnRotation);
+				hazardshoot.transform.Rotate(new Vector3(0,0,1),90);
 				yield return new WaitForSeconds (spawnWait);
+
+		
+				
 			}
 			yield return new WaitForSeconds (waveWait);
 		}
+
 	}
 
 }
