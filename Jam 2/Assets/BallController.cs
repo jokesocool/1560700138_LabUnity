a6fcc8f2cc11c7e3c10345c3c037f908b8391012
WUTTHICHAI KAMMASIT <wutthichai.kamm@bulive.net>
2016-03-30 12:40:15 +0700

    Jam 2 Lab V2

--- Jam 2/Assets/BallController.cs
+++ Jam 2/Assets/BallController.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class BallController : MonoBehaviour {
 	public Rigidbody rb;
 	public GameObject Button;
+	public Button btnGameOver;
 	bool readyJump;
+	public int count = 0;
 
 	public void Jump (){
-		if (readyJump) {
+		if (readyJump || count < 2) {
 			rb.velocity = new Vector3 (0, 5, 0);
+			count++;
 		}
 	}
 	void Start () {
 
 		rb = GetComponent<Rigidbody>();
+		btnGameOver.gameObject.SetActive (false);
 	}
 	
 
@@ -24,6 +28,7 @@ public class BallController : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.tag == "box") {
 			readyJump = true;
+			count = 0;
 		}
 	}
 	void OnCollisionExit(Collision collision) {
